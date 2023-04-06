f357b7f841ce7a1a2191eb4d514a1e654024496f
OlgaShreyn <57268388+OlgaShreyn@users.noreply.github.com>
2023-04-06 21:47:44 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -7,11 +7,9 @@ from scipy.stats import norm, chi2
 chat_id = 368780632 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
     alpha = 1 - p
-    size = len(x)
-    left = np.sqrt(size * (x ** 2).mean() / (11 * chi2.ppf(q=1 - alpha / 2, df=size)))
-    right = np.sqrt(size * (x ** 2).mean() / (11 * chi2.ppf(q=alpha / 2, df=size)))
-    return (left, right)
+    n = len(x)
+    sum = (x**2).sum()
+    left = np.sqrt(sum / (11 * chi2.ppf(1 - alpha / 2, n * 2)))
+    right = np.sqrt(sum / (11 * chi2.ppf(alpha / 2, n * 2)))
+    return left, right
